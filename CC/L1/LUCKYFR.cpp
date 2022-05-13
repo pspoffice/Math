b66c8a42d035aa338e2796a52233ccb11f64a0fa
Priyank S Patel <pspoffice365@gmail.com>
2022-05-14 00:21:20 +0530

    LUCKYFR

---
+++ CC/L1/LUCKYFR.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+#define FAST ios::sync_with_stdio(false), cin.tie(nullptr)
+
+auto Solve() -> void {
+  int x;
+  cin >> x;
+  int answer = 0;
+  while (x) {
+    answer += (x % 10 == 4 ? 1 : 0);
+    x /= 10;
+  }
+  cout << answer << '\n';
+}
+
+auto main() -> int {
+  FAST;
+  int t;
+  cin >> t;
+  while (t--) Solve();
+  return 0;
+}
